83b0a47a10ec73a6789669cac67c2dc08d2ee232
omkargb <omkargb107@gmail.com>
2021-07-19 11:17:34 +0530

    Add : first c# program - Doing addition and subtraction

---
+++ firstProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace firstProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Adition program");
+            Console.Write("Enter first number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int addition = num1 + num2;
+            int subtraction = num1 - num2;
+
+            Console.Write(" Addition is : " + addition);
+            Console.Write("\n Subtraction is : " + subtraction+ "\n");
+        }
+    }
+}
